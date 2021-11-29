b914ccb4bf21d57db175ed041ec2b9b26309cea7
Faik Turan <turanfaik@hotmail.com>
2021-11-29 19:35:30 +0300

    Add project files.

---
+++ MCD_EncapsulationOrnek/Daire.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EncapsulationOrnek
+{
+    class Daire
+    {
+        #region Ozellikler
+        public double YariCap { get; set; }
+
+        private double _cap = 0;
+
+        public double Cap
+        {
+            get
+            {
+                if (YariCap >0d)
+                {
+                    _cap = YariCap * 2;
+                }
+
+                return _cap;
+            }
+        }
+
+        public double PiSayisi { get; set; }
+
+        #endregion
+
+        #region Constructorlar Yani Yapıcı Metotlar
+        public Daire()
+        {
+            PiSayisi = 3;
+        }
+
+        public Daire(double DisardanAlinanPiSayisi)
+        {
+            PiSayisi = DisardanAlinanPiSayisi;
+        }
+        #endregion
+
+
+       
+    }
+}
---
+++ MCD_EncapsulationOrnek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EncapsulationOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Daire isimli bir class'a aşağıdaki şekilde nesne oluşturma kuralları uygulayınız.
+             * Pi sayısı olarak bir özellik tutunuz. Bu özelliğin değerini 3 atayınız.
+             * Pi sayı değerini dışardan gönderiniz.
+             * Bu dairenin çevresini ve alanını hesaplayınız.
+             */
+        }
+    }
+}
